d22e7c9f8ee959c33aef548cdfd92d62f431dc0b
Dan Sell <dansell@dansell-U46E.(none)>
2013-04-29 15:35:23 -0700

    Tweet retrieve working

--- serverhandler.js
+++ serverhandler.js
@@ -3,25 +3,24 @@ var twitter = require('ntwitter'),
 
 var handleRequest = function(req, res) {
 
-  var twit = function() {
-    return new twitter({
+  var twit = new twitter({
       consumer_key: 'lgqxj6MJ66FioUxtEgal9g',
       consumer_secret: '19ExG6Hpx5wYNFZrTJCQrksgBrwaoL5pT5U20WfOZo',
       access_token_key: '19172093-U4mN7t2pdcccs69vEAxjzWHTYeFJjXKqEYJsoMCw',
       access_token_secret: 'uPNVKfT67DpMbWd9Bpw4LVDVzueYWykwlZwxDqkFp34'
-    });
-  };
-
-  twit.verifyCredentials(function (err, data) {
-    console.log("Verifying Credentials...");
-    if(err)
-      console.log("Verification failed : " + err);
   });
 
-  twit.search("$AAPL", {}, function(err, data) {
+  if(req.method === "GET" && url.parse(req.url)) {
+    console.log('url:', url.parse(req.url).pathname);
+    var stockHashes = '$' + url.parse(req.url).pathname.toString().substr(1) + ' OR #' + url.parse(req.url).pathname.toString().substr(1);
+    console.log('stockHashes:', stockHashes);
+    twit.search(stockHashes, {}, function(err, data) {
       console.log(data.results[0].id, data.results[0].text, data.results[0].created_at);
       return data;
-  });
+    });
+  } else {
+    return;
+  }
 
 };
 
--- stockdata.js
+++ stockdata.js
@@ -24,50 +24,91 @@ $(document).ready(function() {
     var selectedText = getSelectedText();
     var selectedNode = getSelectedNode();
     console.log(selectedText, selectedNode);
-    $.when(
-      $.ajax({
-        url: 'http://download.finance.yahoo.com/d/quotes.csv?s=' + selectedText + '&f=n0l1p0&e=.csv',
-        success: function(data)
-        {
-          var dataArray = data.split(',');
-          var stockName = dataArray[0];
-          var lastPrice = dataArray[1];
-          var previousClose = dataArray[2];
-          console.log(previousClose);
-          if(previousClose >= 0) {
-            $('body').append(
-            '<container class="popup">' +
-              '<div>ID: ' + stockName + '</div>' +
-              '<div>Current Price: $' + lastPrice + '<div>' +
-              '<div>Prev Close: $' + previousClose + '<div>' +
-            '</container>'
-            );
-            $('.popup').css('top', e.pageY)
-            .css('left', e.pageX)
-            .css('position', 'absolute')
-            .css('width', 'auto')
-            .css('padding', '10px')
-            .css('background', '#eeeeee')
-            .css('color', '#000000')
-            .css('border', '1px solid #1a1a1a')
-            .css('font-size', '90%');
-          } else {
-            $('body').append(
-            '<container class="popup" style="text-align:center">No Pricing Information Available</container>'
-            );
-            $('.popup').css('top', e.pageY)
-            .css('left', e.pageX)
-            .css('position', 'absolute')
-            .css('width', '280px')
-            .css('padding', '10px')
-            .css('background', '#eeeeee')
-            .css('color', '#000000')
-            .css('border', '1px solid #1a1a1a')
-            .css('font-size', '90%');
-          }
+    $.ajax({
+      url: 'http://download.finance.yahoo.com/d/quotes.csv?s=' + selectedText + '&f=n0l1p0&e=.csv',
+      success: function(data)
+      {
+        var dataArray = data.split(',');
+        var stockName = dataArray[0];
+        var lastPrice = dataArray[1];
+        var previousClose = dataArray[2];
+        console.log(previousClose);
+        if(previousClose >= 0) {
+          $('body').append(
+          '<container class="popup">' +
+            '<div>ID: ' + stockName + '</div>' +
+            '<div>Current Price: $' + lastPrice + '<div>' +
+            '<div>Prev Close: $' + previousClose + '<div>' +
+          '</container>'
+          );
+          $('.popup').css('top', e.pageY)
+          .css('left', e.pageX)
+          .css('position', 'absolute')
+          .css('width', 'auto')
+          .css('padding', '10px')
+          .css('background', '#eeeeee')
+          .css('color', '#000000')
+          .css('border', '1px solid #1a1a1a')
+          .css('font-size', '90%');
+        } else {
+          $('body').append(
+          '<container class="popup" style="text-align:center">No Pricing Information Available</container>'
+          );
+          $('.popup').css('top', e.pageY)
+          .css('left', e.pageX)
+          .css('position', 'absolute')
+          .css('width', '280px')
+          .css('padding', '10px')
+          .css('background', '#eeeeee')
+          .css('color', '#000000')
+          .css('border', '1px solid #1a1a1a')
+          .css('font-size', '90%');
         }
-      });
-    );
+      }
+    });
+    $.ajax({
+      url: 'http://127.0.0.1:8080/' + selectedText,
+      success: function(data)
+      {
+        console.log(data);
+        // var dataArray = data.split(',');
+        // var stockName = dataArray[0];
+        // var lastPrice = dataArray[1];
+        // var previousClose = dataArray[2];
+        // console.log(previousClose);
+        // if(previousClose >= 0) {
+        //   $('body').append(
+        //   '<container class="popup">' +
+        //     '<div>ID: ' + stockName + '</div>' +
+        //     '<div>Current Price: $' + lastPrice + '<div>' +
+        //     '<div>Prev Close: $' + previousClose + '<div>' +
+        //   '</container>'
+        //   );
+        //   $('.popup').css('top', e.pageY)
+        //   .css('left', e.pageX)
+        //   .css('position', 'absolute')
+        //   .css('width', 'auto')
+        //   .css('padding', '10px')
+        //   .css('background', '#eeeeee')
+        //   .css('color', '#000000')
+        //   .css('border', '1px solid #1a1a1a')
+        //   .css('font-size', '90%');
+        // } else {
+        //   $('body').append(
+        //   '<container class="popup" style="text-align:center">No Pricing Information Available</container>'
+        //   );
+        //   $('.popup').css('top', e.pageY)
+        //   .css('left', e.pageX)
+        //   .css('position', 'absolute')
+        //   .css('width', '280px')
+        //   .css('padding', '10px')
+        //   .css('background', '#eeeeee')
+        //   .css('color', '#000000')
+        //   .css('border', '1px solid #1a1a1a')
+        //   .css('font-size', '90%');
+        // }
+      }
+    });
     setTimeout(function() {
       $('.popup').remove();
     }, 5000);
