33a6a92d7d8107dcd7a76a3940ae1e89dd099baf
Dan Sell <dansell@dansell-U46E.(none)>
2013-05-22 12:21:14 -0700

    Edited CSS to be in Objects, created ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 TakeStock
 =========
+TakeStock is a Google Chrome Extension that allows the user to see stock data and recent Tweets just by highlighting any text on a loaded webpage.  Upon highlighting, a popup containing stock pricing information, a stock graph, and a Twitter feed is overlayed on the page.
+
+##Features
+1.  Ability to obtain stock pricing data, both numerical and graphical, as well as recent Tweets, simply by highlighting text on an active webpage.
+
+##Technology
+1.  Node.js
+2.  Twitter API Integration
+3.  Finance.Yahoo.com API Integration
+
+##Installation
+
+####Clone Repo
+`git clone https://github.com/sellds4/TakeStock.git`
+
+####Install Node
+You may obtain pre-compiled Node.js binaries for several platforms from http://nodejs.org/download.
+
+####Install nTwitter
+
+`npm install ntwitter` (https://github.com/AvianFlu/ntwitter)
+
+NOTE:  You will need to register your app with the Twitter API (https://dev.twitter.com/).  Also, to run the server locally after installing Node.js, simply type "node server.js" in the command line ensuring that you have access to the TakeStock local folder.
--- stockdata.js
+++ stockdata.js
@@ -1,4 +1,5 @@
-var tweetCSS = {
+appCSS = {
+  tweetCSS: {
   'font-size':'14px',
   'position':'absolute',
   'width':'425px',
@@ -8,20 +9,47 @@ var tweetCSS = {
   'border-radius':'10px',
   'text-align':'left',
   'z-index':9999
-};
-
-var noinfoCSS = {
+  },
+  noinfoCSS: {
   'text-align': 'center',
   'padding': '10px',
   'background-color': '#eeeeee',
   'width': '250px',
   'border': '1px solid black'
+  },
+  stockdataCSS: {
+  'padding':'10px 0 5px 0',
+  'color': 'blue',
+  'font-size': '125%',
+  'font-weight': 'bold',
+  'text-align': 'center'
+  },
+  stockgraph: {
+    'border-bottom': '2px solid rgba(165, 10, 10, 0.75)',
+    'padding': '10px 0 10px 35px'
+  },
+  tweettext: {
+    'padding': '5px 10px 7px 10px',
+    'font-style': 'italic',
+    'font-weight': 'bold'
+  },
+  tweetuser: {
+    'padding': '0 0 2px 10px',
+    'font-size': '12px'
+  },
+  tweetdate: {
+  'padding': '0 0 5px 10px',
+  'font-size': '11px'
+  },
+  moretweets: {
+    'padding': '5px',
+    'text-align': 'right',
+    'background-color': '#FFFDD0',
+    'border-top': '2px solid rgba(165, 10, 10, 0.75)'
+  }
 };
 
-var stockdataCSS = {
-  'padding':'10px 0 5px 0'
-};
-
+// Determines the selected text that is sent to the backend for stock data
 var getSelectedText = function() {
     var text = "";
     if (window.getSelection) {
@@ -32,6 +60,7 @@ var getSelectedText = function() {
     return text;
 };
 
+// Can select the DOM node that the selected text resides in (currently not in use in the extension)
 var getSelectedNode = function() {
     if (document.selection)
       return document.selection.createRange().parentElement();
@@ -43,6 +72,7 @@ var getSelectedNode = function() {
     }
 };
 
+// Appends tweets to pop-up
 var tweetGet = function(data, firstTweet, lastTweet) {
   var color;
   for(var i = firstTweet; i < lastTweet; i++){
@@ -53,17 +83,23 @@ var tweetGet = function(data, firstTweet, lastTweet) {
     }
     $('.popup').append(
       '<container class="tweet">' +
-        '<div class="tweettext" style="padding:5px 10px 7px 10px; font-style:italic; background-color:' + color + '"><strong>' + data[i].text + '</strong></div>' +
-        '<div class="tweetuser" style="padding:0 0 2px 10px; font-size:12px;background-color:' + color + '">- ' + data[i].from_user + '</div>' +
-        '<div class="tweetdate" style="padding:0 0 5px 10px; font-size:11px;background-color:' + color + '">' + data[i].created_at + '</div>' +
+        '<div class="tweettext" style="background-color:' + color + '">' + data[i].text + '</div>' +
+        '<div class="tweetuser" style="background-color:' + color + '">- ' + data[i].from_user + '</div>' +
+        '<div class="tweetdate" style="background-color:' + color + '">' + data[i].created_at + '</div>' +
       '</container>'
     );
+    $('.tweettext').css(appCSS.tweettext);
+    $('.tweetuser').css(appCSS.tweetuser);
+    $('.tweetdate').css(appCSS.tweetdate);
   }
+  // More tweets feature to be added later
   // $('.popup').append(
-  //   '<div clas s="moretweets" style="padding:5px; text-align:right; background-color:#FFFDD0; border-top:2px solid rgba(165, 10, 10, 0.75)"><a href="#">More Tweets</a></div>'
+  //   '<div class="moretweets"><a href="#">More Tweets</a></div>'
   // );
+  // $('.moretweets').css(appCSS.moretweets);
 };
 
+// Item below is an index for finance.yahoo.com data array
 //n=Name[0], s=symbol[1], l1=LastTrade[2], p=PreviousClose[3], o=Open[4], b=Bid[5], a=Ask[6], g=Day'sLow[7], h=Day'sHigh[8]
 
 $(document).ready(function() {
@@ -98,20 +134,21 @@ $(document).ready(function() {
         } else if(previousClose >= 0) {
           $('body').append(
           '<container class="popup">' +
-            '<div class="stockdata" style="color:blue; font-size:125%; text-align:center"><strong>' + stockName + ' (' + stockSymbol + ')</strong></div>' +
+            '<div class="stockdata">' + stockName + ' (' + stockSymbol + ')</div>' +
             '<div><table width=100%>' +
             '<tr><th>Current Price:</th><td style="color:' + priceColor + '">$' + lastPrice + '</td><th>Current Bid:</th><td>$' + bid + '</td></tr>' +
             '<tr><th>Today&#39s Open:</th><td>$' + openPrice + '</td><th>Current Ask:</th><td>$' + ask + '</td></tr>' +
             '<tr><th>Previous Close:</th><td>$' + previousClose + '</td><th>Day&#39s Range:</th><td>$' + daysLow + '-$' + daysHigh + '</td></tr>' +
             '</table></div>' +
-            '<div class="stockgraph" style="border-bottom:2px solid rgba(165, 10, 10, 0.75); padding:10px 0 10px 35px"><img src="http://chart.finance.yahoo.com/z?s=' + selectedText + '&t=6m&q=l&l=on&z=s&p=m50,m200"></div>' +
+            '<div class="stockgraph"><img src="http://chart.finance.yahoo.com/z?s=' + selectedText + '&t=6m&q=l&l=on&z=s&p=m50,m200"></div>' +
           '</container>'
           );
-          $('.popup').css(tweetCSS)
+          $('.popup').css(appCSS.tweetCSS)
           .css('top', mouseY)
           .css('left', mouseX);
           $('.popup').find('th').css({'padding-left':'10px'});
-          $('.stockdata').css(stockdataCSS);
+          $('.stockgraph').css(appCSS.stockgraph);
+          $('.stockdata').css(appCSS.stockdataCSS);
           $.ajax({
             url: 'http://127.0.0.1:8080/' + selectedText,
             error: function(){ console.log('Error'); },
@@ -126,10 +163,10 @@ $(document).ready(function() {
           $('body').append(
           '<container class="popup" style="text-align:center">No Pricing Information Available</container>'
           );
-          $('.popup').css(tweetCSS)
+          $('.popup').css(appCSS.tweetCSS)
           .css('top', e.pageY)
           .css('left', e.pageX)
-          .css(noinfoCSS);
+          .css(appCSS.noinfoCSS);
         }
       }
     });
@@ -137,15 +174,16 @@ $(document).ready(function() {
       $('.popup').remove();
       $('.moretweets').remove();
     });
-    $('.moretweets').click(function(e) {
-      console.log("More Tweets");
-      e.preventDefault();
-      e.stopImmediatePropagation();
-      $('.tweet').remove();
-      firstTweetIndex += 5;
-      lastTweetIndex += 5;
-      console.log(firstTweetIndex, lastTweetIndex);
-      tweetGet(data, firstTweetIndex, lastTweetIndex);
-    });
+    // More tweets feature to be added later
+    // $('.moretweets').click(function(e) {
+    //   console.log("More Tweets");
+    //   e.preventDefault();
+    //   e.stopImmediatePropagation();
+    //   $('.tweet').remove();
+    //   firstTweetIndex += 5;
+    //   lastTweetIndex += 5;
+    //   console.log(firstTweetIndex, lastTweetIndex);
+    //   tweetGet(data, firstTweetIndex, lastTweetIndex);
+    // });
   });
 });
